693b77cdd05ff9d85b99ac3e8342ac21048b7589
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-05-09 14:12:38 +0200

    Pola na liście i filtr

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'isbn')
+    list_filter = ('publisher',)
+
 # Register your models here.
 admin.site.register(Publisher)
 admin.site.register(Contributor)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review)
