d4136a0d9b5a27ccf43d73ae5830b8a370da9d70
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-05-10 18:31:07 +0200

    Wykluczanie i grupowanie pól

--- reviews/admin.py
+++ reviews/admin.py
@@ -7,9 +7,14 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'isbn')
     list_filter = ('publisher', 'publication_date')
 
+class ReviewAdmin(admin.ModelAdmin):
+    exclude = ('date_edited',)
+    fieldsets = ((None,{'fields': ('creator', 'book')}),
+                 ('Zawartość recenzji',{'fields': ('content', 'rating')}))
+
 # Register your models here.
 admin.site.register(Publisher)
 admin.site.register(Contributor)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
