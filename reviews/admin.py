2ec8b8ed079cf97ae126685391717a31c060b81c
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-05-10 18:19:01 +0200

    Filtr hierarchii dat oraz pasek wyszukiwania

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
 class BookAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'isbn', 'publisher__name')
+    date_hierarchy = 'publication_date'
     list_display = ('title', 'isbn')
     list_filter = ('publisher', 'publication_date')
 
