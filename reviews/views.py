2479ea62ba4a5a7722035bd195ac5b2308fb1794
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-05-30 15:35:26 +0200

    Stylowanie i integrowanie formularza modelu Publisher z witryną

--- reviews/views.py
+++ reviews/views.py
@@ -89,4 +89,4 @@ def publisher_edit(request, pk=None):
     else:
         form = PublisherForm(instance=publisher)
 
-    return render(request, "form-example.html", {"method": request.method, "form": form})
+    return render(request, "reviews/instance-form.html", {"form": form, "instance": publisher, "model_type": "Publisher"})
