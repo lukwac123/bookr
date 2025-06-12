b11444ee1ce52d020161ff9cb06d0dbb2ddaf9c6
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-06-12 15:54:17 +0200

    Tworzenie niestandardowej witryny administracyjnej

--- bookr/settings.py
+++ bookr/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bookr.apps.ReviewsAdminConfig',
+    'bookr_admin.apps.BookrAdminConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
---
+++ bookr_admin/apps.py
@@ -0,0 +1,8 @@
+from django.contrib.admin.apps import AdminConfig
+
+class BookrAdminConfig(AdminConfig):
+    default_site = 'bookr_admin.admin.BookrAdmin'
+
+
+
+
