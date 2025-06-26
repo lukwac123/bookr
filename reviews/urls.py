3ae0b891af198ef0fc2484d5db131b7483987f8c
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-06-26 09:49:43 +0200

    Utworzenie prostego API REST-owego

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views, api_views
 
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('publishers/new/', views.publisher_edit, name='publisher_create'),
     path('books/<int:book_pk>/reviews/new/', views.review_edit, name="review_create"),
     path('books/<int:book_pk>/reviews/<int:review_pk>/', views.review_edit, name="review_edit"),
-    path('books/<int:pk>/media/', views.book_media, name='book_media')
+    path('books/<int:pk>/media/', views.book_media, name='book_media'),
+    path('api/first_api_view', api_views.first_api_view)
 ]
