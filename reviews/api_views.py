0802fc8948282e7f466b79ce9269cff959296822
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-06-26 18:39:46 +0200

    Zdjęcie uwierzytelniania

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -72,8 +72,8 @@ class Login(APIView):
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    authentication_classes = [TokenAuthentication] 
-    permission_classes = [IsAuthenticated]
+    authentication_classes = [] # TokenAuthentication
+    permission_classes = [] # IsAuthenticated
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
