f2c17edc63a90f2c2209aad6726c260a63a91d06
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-06-26 10:20:37 +0200

    Utworzenie widoku API opartego na klasach iserializerach modeli

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from .models import Book, Publisher
 
+"""
 class PublisherSerializer(serializers.Serializer):
     name = serializers.CharField()
     website = serializers.URLField()
@@ -10,3 +12,15 @@ class BookSerializer(serializers.Serializer):
     publication_date = serializers.DateField()
     isbn = serializers.CharField()
     publisher = PublisherSerializer()
+"""
+
+class PublisherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = ['name', 'website', 'email']
+
+class BookSerializer(serializers.ModelSerializer):
+    publisher = PublisherSerializer()
+    class Meta:
+        model = Book
+        fields = ['title', 'publication_date', 'isbn', 'publisher']
