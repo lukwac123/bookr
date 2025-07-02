5acbba73d291da6eccd21047c25be5c46d2dcef7
Łukasz Wacławek <lukasz-waclawek@wp.pl>
2025-07-02 14:48:47 +0200

    Korzystanie ze składni JSX i Babel

--- static/react-example.js
+++ static/react-example.js
@@ -1,16 +1,16 @@
-const e = React.createElement;
-
 class ClickCounter extends React.Component {
     constructor(props) {
         super(props);
         this.state = { clickCount: 0 };
     }
-    render(){
-        return e(
-            'button', {onClick: () => this.setState({clickCount: this.state.clickCount + 1})},
-            this.state.clickCount
-        );
+    render() {
+        return <button onClick={() => this.setState({
+            clickCount: this.state.clickCount + 1
+        })
+    }>
+        {this.state.clickCount}
+    </button>;
     }
 }
 
-ReactDOM.render(e(ClickCounter), document.getElementById('react_container'))
+ReactDOM.render(<ClickCounter />, document.getElementById('react_container'))
